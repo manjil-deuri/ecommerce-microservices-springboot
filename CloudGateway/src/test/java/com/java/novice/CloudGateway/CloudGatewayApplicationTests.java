8d9f54df7e7252d00a947c8e55a2dee89efc721d
MANJIL DEURI <manjil.d@gmail.com>
2024-09-29 17:13:37 +0530

    Consolidated Repo

---
+++ CloudGateway/src/test/java/com/java/novice/CloudGateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.novice.CloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
